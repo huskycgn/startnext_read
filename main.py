fc8ef103f2c5acb9d951ebd9d9f9c020f488a98f
joachimlehmann <joachim.lehmann@googlemail.com>
2023-03-15 14:03:26 +0100

    inserted timestamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from funcs import get_startnext_amount, send_message
 from time import sleep
+from datetime import datetime
 
 startvalue = get_startnext_amount()
 print(startvalue)
@@ -7,6 +8,7 @@ while True:
     newvalue = get_startnext_amount()
     if newvalue != startvalue:
         send_message(f'Betrag ist jetzt {newvalue} €')
+    print(datetime.now())
     print(f'Start value: {startvalue} €')
     print(f'Current value: {newvalue} €')
     startvalue = newvalue
